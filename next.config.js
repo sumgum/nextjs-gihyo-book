9f1e3d1dfc898e865ecc2f4a85c2f5da9c8f8e0a
Sumi Gunyu <sumkakkyo@gmail.com>
2022-09-16 10:09:54 +0900

    setting dev

--- next.config.js
+++ next.config.js
@@ -2,9 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  compiler: {
-    styledComponents: true,
-  },
+  compiler: (() => {
+    let compilerConfig = {
+      styledComponents: true,
+    };
+
+    if (process.env.NODE_ENV === 'production') {
+      compilerConfig = {
+        ...compilerConfig,
+        // 本番環境ではReact Testing Libraryで使用するdata-testid属性を削除
+        reactRemoveProperties: { properties: ['^data-testid$'] },
+      };
+    }
+
+    return compilerConfig;
+  })(),
 };
 
 module.exports = nextConfig;
