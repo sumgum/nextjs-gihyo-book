49bba8402c9d5cef803fa56663e444b56ac0f3a8
Sumi Gunyu <sumkakkyo@gmail.com>
2022-09-17 19:33:50 +0900

    implement Auth API

---
+++ src/types/data.d.ts
@@ -0,0 +1,32 @@
+// 商品のカテゴリ
+export type Category = 'shoes' | 'clothes' | 'book';
+// 商品の状態
+export type Condtition = 'new' | 'used';
+
+// ユーザー
+export type User = {
+  id: number;
+  username: string;
+  displayName: string;
+  email: string;
+  profileImageUrl: string;
+  description: string;
+};
+
+// 商品
+export type Product = {
+  id: number;
+  category: Category;
+  title: string;
+  description: string;
+  imageUrl: string;
+  blurDataUrl: string;
+  price: number;
+  condition: Condtition;
+  owner: User;
+};
+
+// APIContext
+export type ApiContext = {
+  apiRootUrl: string;
+};
