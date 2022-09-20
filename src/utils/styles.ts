0b6f135e2b03f4935ea2752f8890de9266c3d2d0
Sumi Gunyu <sumkakkyo@gmail.com>
2022-09-20 10:15:59 +0900

    create theme

---
+++ src/utils/styles.ts
@@ -0,0 +1,180 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/ban-types */
+
+import { theme } from 'themes';
+import type { ResponsiveProp, Responsive } from 'types';
+
+// Themeの型
+export type AppTheme = typeof theme;
+
+type SpaceThemeKeys = keyof typeof theme.space;
+type FontSizeThemeKeys = keyof typeof theme.fontSizes;
+type LetterSpacingThemeKeys = keyof typeof theme.letterSpacing;
+type LineHeightsThemeKeys = keyof typeof theme.lineHeights;
+type ColorThemeKeys = keyof typeof theme.colors;
+
+// 各Themeのキーの型
+export type Space = SpaceThemeKeys | (string & {});
+export type FontSize = FontSizeThemeKeys | (string & {});
+export type LetterSpacing = LetterSpacingThemeKeys | (string & {});
+export type LineHeights = LineHeightsThemeKeys | (string & {});
+export type Color = ColorThemeKeys | (string & {});
+
+// ブレークポイント
+const BREAKPOINTS: { [key: string]: string } = {
+  sm: '640px',
+  md: '768px',
+  lg: '1024px',
+  xl: '1280px',
+};
+
+/**
+ * Responsive型をCSSプロパティとその値に変換
+ * @param propKey CSSプロパティ
+ * @param prop Responsive型
+ * @param theme AppTheme
+ * @returns CSSプロパティとその値（ex. background-color: white;）
+ */
+export function toPropValue<T>(
+  propKey: string,
+  prop?: Responsive<T>,
+  theme?: AppTheme,
+) {
+  if (prop === undefined) return undefined;
+
+  if (isResponsivePropType(prop)) {
+    const result = [];
+    for (const responsiveKey in prop) {
+      if (responsiveKey === 'base') {
+        result.push(
+          `${propKey}: ${toThemeValueIfNeeded(
+            propKey,
+            prop[responsiveKey],
+            theme,
+          )}`,
+        );
+      } else if (
+        responsiveKey === 'sm' ||
+        responsiveKey === 'md' ||
+        responsiveKey === 'lg' ||
+        responsiveKey === 'xl'
+      ) {
+        // メディアクエリでのスタイル
+        const breakpoint = BREAKPOINTS[responsiveKey];
+        const style = `${propKey}: ${toThemeValueIfNeeded(
+          propKey,
+          prop[responsiveKey],
+          theme,
+        )}`;
+        result.push(`@media screen and (min-width: ${breakpoint}) {${style}}`);
+      }
+    }
+    return result.join('\n');
+  }
+  return `${propKey}: ${toThemeValueIfNeeded(propKey, prop, theme)}`;
+}
+const SPACE_KEYS = new Set([
+  'margin',
+  'margin-top',
+  'margin-left',
+  'margin-bottom',
+  'margin-right',
+  'padding',
+  'padding-top',
+  'padding-left',
+  'padding-bottom',
+  'padding-right',
+]);
+
+const COLOR_KEYS = new Set(['color', 'background-color']);
+const FONT_SIZE_KEYS = new Set(['font-size']);
+const LINE_SPACING_KEYS = new Set(['letter-spacing']);
+const LINE_HEIGHT_KEYS = new Set(['line-height']);
+
+/**
+ * Themeに指定されたCSSプロパティの値に変換
+ * @param propKey CSSプロパティ
+ * @param value CSSプロパティの値
+ * @param theme AppTheme
+ * @returns CSSプロパティの値
+ */
+function toThemeValueIfNeeded<T>(propKey: string, value: T, theme?: AppTheme) {
+  if (
+    theme &&
+    theme.space &&
+    SPACE_KEYS.has(propKey) &&
+    isSpaceThemeKeys(value, theme)
+  ) {
+    return theme.space[value];
+  } else if (
+    theme &&
+    theme.colors &&
+    COLOR_KEYS.has(propKey) &&
+    isColorThemeKeys(value, theme)
+  ) {
+    return theme.colors[value];
+  } else if (
+    theme &&
+    theme.fontSizes &&
+    FONT_SIZE_KEYS.has(propKey) &&
+    isFontSizeThemeKeys(value, theme)
+  ) {
+    return theme.fontSizes[value];
+  } else if (
+    theme &&
+    theme.letterSpacing &&
+    LINE_SPACING_KEYS.has(propKey) &&
+    isLetterSpacingThemeKeys(value, theme)
+  ) {
+    return theme.letterSpacing[value];
+  } else if (
+    theme &&
+    theme.lineHeights &&
+    LINE_HEIGHT_KEYS.has(propKey) &&
+    isLineHeightThemeKeys(value, theme)
+  ) {
+    return theme.lineHeights[value];
+  }
+
+  return value;
+}
+
+function isResponsivePropType<T>(prop: any): prop is ResponsiveProp<T> {
+  return (
+    prop &&
+    (prop.base !== undefined ||
+      prop.sm !== undefined ||
+      prop.md !== undefined ||
+      prop.lg !== undefined ||
+      prop.xl !== undefined)
+  );
+}
+
+function isSpaceThemeKeys(prop: any, theme: AppTheme): prop is SpaceThemeKeys {
+  return Object.keys(theme.space).filter((key) => key == prop).length > 0;
+}
+
+function isColorThemeKeys(prop: any, theme: AppTheme): prop is ColorThemeKeys {
+  return Object.keys(theme.colors).filter((key) => key == prop).length > 0;
+}
+
+function isFontSizeThemeKeys(
+  prop: any,
+  theme: AppTheme,
+): prop is FontSizeThemeKeys {
+  return Object.keys(theme.fontSizes).filter((key) => key == prop).length > 0;
+}
+function isLetterSpacingThemeKeys(
+  prop: any,
+  theme: AppTheme,
+): prop is LetterSpacingThemeKeys {
+  return (
+    Object.keys(theme.letterSpacing).filter((key) => key == prop).length > 0
+  );
+}
+function isLineHeightThemeKeys(
+  prop: any,
+  theme: AppTheme,
+): prop is LineHeightsThemeKeys {
+  return Object.keys(theme.lineHeights).filter((key) => key == prop).length > 0;
+}
