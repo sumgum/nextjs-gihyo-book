4256cc583e5f4b374a99845d46d9e0292c11029a
Sumi Gunyu <sumkakkyo@gmail.com>
2022-09-20 15:38:52 +0900

    atoms ShapeImage

---
+++ src/components/atoms/ShapeImage/index.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+import Image, { ImageProps } from 'next/image';
+
+type ImageShape = 'circle' | 'square';
+type ShapeImageProps = ImageProps & { shape?: ImageShape };
+
+// circleなら円形に
+const ImageWithShape = styled(Image)<{ shape?: ImageShape }>`
+  border-radius: ${({ shape }) => (shape === 'circle' ? '50%' : '0')};
+`;
+
+/**
+ * シェイプイメージ
+ */
+const ShapeImage = (props: ShapeImageProps) => {
+  const { shape, ...imageProps } = props;
+
+  return <ImageWithShape shape={shape} {...imageProps} />;
+};
+
+export default ShapeImage;
