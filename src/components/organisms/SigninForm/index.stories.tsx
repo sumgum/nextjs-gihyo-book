2b096057fc3002d86a86776ca5f27fc9e4b53538
Sumi Gunyu <sumkakkyo@gmail.com>
2022-09-30 19:43:01 +0900

    organisms SigninForm

---
+++ src/components/organisms/SigninForm/index.stories.tsx
@@ -0,0 +1,20 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import SigninForm from '.';
+
+export default {
+  title: 'Organisms/SigninForm',
+  argTypes: {
+    onSignin: {
+      description: 'サインインボタンを押したときのイベントハンドラ',
+      table: {
+        type: { summary: 'function' },
+      },
+    },
+  },
+} as ComponentMeta<typeof SigninForm>;
+
+const Template: ComponentStory<typeof SigninForm> = (args) => (
+  <SigninForm {...args} />
+);
+
+export const Form = Template.bind({});
